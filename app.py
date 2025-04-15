1c1db2ba829223014f76333af67647f6a5b4cd75
Ricardo Mazzarioli <ricardomazzarioli@outlook.com>
2025-04-15 16:08:52 -0300

    Correção serviço

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from waitress import serve
 from routes import registrar_rotas
 
 app = Flask(__name__)
 registrar_rotas(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app, host='0.0.0.0', port=80)
